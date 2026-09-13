06187714f6eac1abeff15b8cb80ab6bbf0378af4
JAYJAYTGG <155000328+JJTGG@users.noreply.github.com>
2026-09-13 14:20:59 +0100

    add: window dragging (mouse + touch)

--- script.js
+++ script.js
@@ -26,3 +26,43 @@ notesLink.addEventListener('click', () => {
 notesWindow.querySelector('.close-btn').addEventListener('click', () => {
   notesWindow.classList.add('hidden');
 });
+
+function makeDraggable(windowEl) {
+  const header = windowEl.querySelector('.window-header');
+  let offsetX = 0;
+  let offsetY = 0;
+  let isDragging = false;
+
+  function startDrag(x, y) {
+    isDragging = true;
+    const rect = windowEl.getBoundingClientRect();
+    offsetX = x - rect.left;
+    offsetY = y - rect.top;
+  }
+
+  function moveDrag(x, y) {
+    if (!isDragging) return;
+    windowEl.style.left = `${x - offsetX}px`;
+    windowEl.style.top = `${y - offsetY}px`;
+  }
+
+  function endDrag() {
+    isDragging = false;
+  }
+
+  header.addEventListener('mousedown', (e) => startDrag(e.clientX, e.clientY));
+  document.addEventListener('mousemove', (e) => moveDrag(e.clientX, e.clientY));
+  document.addEventListener('mouseup', endDrag);
+
+  header.addEventListener('touchstart', (e) => {
+    const touch = e.touches[0];
+    startDrag(touch.clientX, touch.clientY);
+  });
+  document.addEventListener('touchmove', (e) => {
+    const touch = e.touches[0];
+    moveDrag(touch.clientX, touch.clientY);
+  });
+  document.addEventListener('touchend', endDrag);
+}
+
+makeDraggable(notesWindow);
